7338ba25e05cf71f63468517a2c521efcb0cab17
maria-roman-garcia <mromangarcia04@gmail.com>
2020-02-10 15:07:27 +0100

    cambios maria

---
+++ src/FuentesInformacion/FuentesInformacion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FuentesInformacion = () => {
+    return(
+        <div className="row">
+            <div className="col-3 col-md-12"></div>
+        </div>
+    )
+}
+
+export default FuentesInformacion;
