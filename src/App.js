e036b712585c1aab0db230b0eea25700a528fc8e
maria-roman-garcia <mromangarcia04@gmail.com>
2020-02-12 10:06:23 +0100

    nos la jugamos por 5 vez. Maldito git

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import './App.scss';
 import HeaderBusqueda from './Header/HeaderBusqueda/HeaderBusqueda';
+import FuentesInformacion from './FuentesInformacion/FuentesInformacion';
  
 
 class App extends Component {
   render(){
      return (
       <div className="App">
-        
+        <FuentesInformacion/>
       </div>
     );
   }
