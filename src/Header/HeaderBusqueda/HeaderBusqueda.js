88dbd6bbd00c6134cb23589a2c87e71160c619e0
Natalia T <tutornatalia22@gmail.com>
2020-02-14 16:50:44 +0100

    subiendo actualizacion con icono

--- src/Header/HeaderBusqueda/HeaderBusqueda.js
+++ src/Header/HeaderBusqueda/HeaderBusqueda.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Button} from 'reactstrap';
 import './HeaderBusqueda.css';
+import FontAwesome from 'react-fontawesome';
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 
 //Clase Header
@@ -14,20 +18,27 @@ const HeaderBusqueda = ()=>{
                 
                 <div className="col-12 col-md-4 justify-content">
                     
-                    <input type="search" placeholder="#" className="BarraBusqueda"/>
-                    
+                    <input type="search" className="BarraBusqueda"/>
+                    <FontAwesomeIcon icon={faHashtag} className="Icono" />
                 </div>
                 
                 <div className="col-12 col-md-4 justify-content">
                     <button className="MyButton" onclick="myFunction()">Buscar</button>
                 </div>
-                    
+                
             </div>
         )
+    }
 
-    
-   
+        export default HeaderBusqueda;
 
-}
+        
+        
+        // class Question extends React.Component {
+        //     render() {
+        //         return <h3> Lets go for a <FaBeer />? </h3>
+        //     }
+        // }
+            
+            
 
-export default HeaderBusqueda;
