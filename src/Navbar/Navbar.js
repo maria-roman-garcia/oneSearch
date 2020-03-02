0ec9ad1bf2b72e8db6dacc81461285dd2a2358ab
Natalia T <tutornatalia22@gmail.com>
2020-03-02 14:17:33 +0100

    div arreglado de navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Navbar.scss'
 import Modal from '../Modal/Modal'
 
@@ -9,36 +9,38 @@ import Modal from '../Modal/Modal'
 
 class Navbar extends Component {
 
-    componentDidMount(){
-        document.getElementById('nav-menu-hamburger').addEventListener('click', (event)=>{
-                event.preventDefault();
-                document.getElementById('nav-menu-hamburger').classList.toggle('active')
-                document.getElementsByClassName('nav-menu-body')[0].classList.toggle('hidden')
-            })        
+    componentDidMount() {
+        document.getElementById('nav-menu-hamburger').addEventListener('click', (event) => {
+            event.preventDefault();
+            document.getElementById('nav-menu-hamburger').classList.toggle('active')
+            document.getElementsByClassName('nav-menu-body')[0].classList.toggle('hidden')
+        })
     }
 
-    render(){
-    return(
-                    
-                        <div className='nav-menu '>
-                            <div className='nav-menu-body hidden'>
-                                <ul>
-                                    <li>TUTORIAL</li>
-                                    <li>GITHUB</li>
-                                    <li><Modal/></li>
-                                </ul>
-                            </div>
-                            <p id='nav-menu-hamburger'>
-                                <span></span>
-                            </div>
-                            </div>
-                    <div className='main-container '></div>
+    render() {
+        return (
+            <div className='main-container '>
+                <div className='nav-menu '>
+                    <div className='nav-menu-body hidden'>
+                        <ul>
+                            <li>TUTORIAL</li>
+                            <li>GITHUB</li>
+                            <li><Modal /></li>
+                        </ul>
+                    </div>
+                    <p id='nav-menu-hamburger'>
+                        <span></span>
+                    </p>
                 </div>
+
+            </div>
+                
+                        
                      
-            )            
+            )
     }
 
-    
+
 }
 
 export default Navbar;
