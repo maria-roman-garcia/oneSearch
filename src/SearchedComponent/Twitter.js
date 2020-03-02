9b67f0e60c18b2c657d7679bff9a5c9f90527a38
maria-roman-garcia <mromangarcia04@gmail.com>
2020-03-02 15:47:29 +0100

    pruebas twitter

--- src/SearchedComponent/Twitter.js
+++ src/SearchedComponent/Twitter.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Twitter.scss'
 
-function Twitter(props) {
-    return(
-        <div className="twitt">
-            <p>Twitter</p>
-        </div> 
-    )
+class Twitter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // twitter
+            infoLlamadaApi: [],
+            inputPalabraBuscar: props.inputPalabraBuscar,
+        }
+    }
+
+    callAPI(palabra) {
+        console.log('llamando API')
+        fetch("http://localhost:9000/testAPI/" + palabra)
+            .then(res => res.json())
+            .then(res => this.setState({
+                infoLlamadaApi: res
+            }))
+            .catch(err => err)
+        setTimeout(() => {
+            console.log(this.state.infoLlamadaApi)
+        }, 2000)
+    }
+
+    componentDidMount() {
+        this.callAPI(this.state.inputPalabraBuscar);
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.inputPalabraBuscar != this.state.inputPalabraBuscar) {
+            this.callAPI(newProps.inputPalabraBuscar);
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div className="twitt">
+                <p>Twitter</p>
+            </div>
+        )
+    }
+
 }
 
 export default Twitter;
