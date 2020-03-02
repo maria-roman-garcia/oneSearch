cf5ae9407b315a80ee5ccf45fee612c43d8edb80
maria-roman-garcia <mromangarcia04@gmail.com>
2020-03-02 10:15:21 +0100

    nuevo gif para la aprte de Otros...estamos trabajando en ello

--- src/SearchedComponent/Otros.js
+++ src/SearchedComponent/Otros.js
@@ -4,6 +4,12 @@ import './Otros.scss'
 function Otros(){
     return(
         <div className="imageWorkingOnIt">
+            <div className="row  justify_center">
+                <p>Estamos trabajando en ello...Estad atent@s!</p>
+            </div>
+            <div className="row  justify_center">
+                <img src="https://media.giphy.com/media/3oEdv1EbS2Ss1NvrUc/giphy.gif" alt="Working on it"/>
+            </div>
         </div>
     )
 }
